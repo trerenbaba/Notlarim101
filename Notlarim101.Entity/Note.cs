752f07264ef6a84741f86a0fb1cf3d4ba7a7cd18
trerenbaba <babaeren57@gmail.com>
2021-12-13 14:49:33 +0300

    Katmanlar ve Entity oluşturuldu. 13.12.2021 gün sonu

---
+++ Notlarim101.Entity/Note.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notlarim101.Entity
+{
+    [Table("tblNotes")]
+    public class Note:MyEntityBase
+    {
+        [StringLength(60),Required]
+        public string Title { get; set; }
+        [Required,StringLength(2000)]
+        public string Text { get; set; }
+        public bool IsDraft { get; set; }
+        public int LikeCount { get; set; }
+
+        public virtual NotlarimUser Owner { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Liked> Likes { get; set; }
+
+
+    }
+}
---
+++ Notlarim101.Entity/NotlarimUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notlarim101.Entity
+{
+    [Table("tblNotlarimUsers")]
+    public class NotlarimUser:MyEntityBase
+    {
+        [StringLength(30)]
+        public string Name { get; set; }
+        [StringLength(30)]
+        public string Surname { get; set; }
+        [StringLength(30),Required]
+        public string UserName { get; set; }
+        [StringLength(100),Required]
+        public string Email { get; set; }
+        [StringLength(100),Required]
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        [Required]
+        public Guid ActivateGuid { get; set; }
+        public bool IsAdmin { get; set; }
+        public virtual List<Note> Notes { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Liked> Likes { get; set; }
+
+    }
+}
